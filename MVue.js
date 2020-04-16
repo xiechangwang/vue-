ced65684006aaee08af12332b1edc59a53f707a3
xiechangwang <1113868253@qq.com>
2020-04-16 18:06:54 +0800

    代码提交。。。。

--- MVue.js
+++ MVue.js
@@ -1,6 +1,21 @@
 class Compile{
 	constructor(el,vm){
 		this.el=this.isElementNode(el)?el:document.querySelector(el);
+		// console.log(this.el);
+		this.vm=vm;
+		//1、获取文档碎片对象，放入内存中会减少页面的回流和重绘
+		const fragment=this.node2Fragment(this.el);
+	}
+	node2Fragment(el){
+		//创建文档碎片对象
+		const f=document.createDocumentFragment();
+		let firstChild;
+		while(firstChild = el.firstChild){
+			f.appendChild(firstChild)
+		}	
+	}
+	isElementNode(node){
+		return node.nodeType===1;
 	}
 }
 
